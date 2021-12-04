35659f96d02dcc974e418efe8226f05e0bd726db
KingHollow <381639464@qq.com>
2021-12-04 18:08:42 +0800

    LYW

---
+++ QingYue_web/src/com/javaweb/qingyue/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.javaweb.qingyue.dao;
+
+
+import com.javaweb.qingyue.entity.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserDao {
+    public boolean login(String name,String password);//登录
+    public boolean register(User user) throws SQLException;//注册
+    public List<User> getUserAll();//返回用户信息集合
+    public boolean delete(int id) ;//根据id删除用户
+    public boolean update(int id, String name, String password, String nickname, String sex, String region, String headshoturl, String signature) ;//更新用户信息
+}
